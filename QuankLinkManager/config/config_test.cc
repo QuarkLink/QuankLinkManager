0c212fb61ee11cfa66a8685090c9ee646712e827
Flash-LHR <2479082762@qq.com>
2025-06-03 22:11:58 +0800

    test

---
+++ QuankLinkManager/config/config_test.cc
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+namespace {
+// This test case only for test the build system.
+// Remove this test case after any other test cases are added.
+TEST(ConfigTest, Test) { EXPECT_EQ(1, 1); }
+}  // namespace
